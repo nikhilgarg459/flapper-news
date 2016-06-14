ef0a27bfac7277b2d85ed241828e34bbd3d92866
Nikhil Garg <nikhilgarg72@gmail.com>
2016-06-14 22:44:45 +0530

    Comments working

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,8 @@ router.get('/posts', function(req,res,next){
 	});
 });
 
+
+/* Create Post */
 router.post('/posts',function(req, res, next){
 	var post = new Post(req.body);
 
@@ -28,6 +30,83 @@ router.post('/posts',function(req, res, next){
 
 		res.json(post);
 	});
+});
+
+/* Preloading post bojects in routes/index.js */
+router.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+
+	query.exec(function (err, post){
+		if(err) { return next(err); }
+		if(!post) { return next(new Error('can\'t find post')); }
+		req.post = post;
+		return next();
+	});
+});
+
+/* Get Post by Id */
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+/* Upvote Post*/
+router.put('/posts/:post/upvote', function(req, res, next){
+	req.post.upvote(function(err, post){
+		if(err) { return next(err);}
+
+		res.json(post);
+	});
+});
+
+/* Creating comments for partivular post */
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+/* Preloading comment bojects in routes/index.js */
+router.param('comment', function(req, res, next, id){
+	var query = Comment.findById(id);
+
+	query.exec(function (err, comment){
+		if(err) { return next(err); }
+		if(!comment) { return next(new Error('can\'t find post')); }
+		req.comment = comment;
+		return next();
+	});
+});
+
+/* Upvote Comment*/
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+	req.comment.upvote(function(err, comment){
+		if(err) { return next(err);}
+
+		res.json(comment);
+	});
+});
+
+router.get('/posts/:post/comments/:comment', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(req.comment);
+  });
 });
 
 module.exports = router;
